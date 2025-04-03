5101e78bff997eda102d746899827536bcc4f626
SurajSingh-Neptron <suraj.s@neptrontech.com>
2025-04-03 18:06:23 +0530

    added tooltip,date range,checkbox

--- src/Services/graph.service.ts
+++ src/Services/graph.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { GraphData } from '../Models/Graph'; 
+import { DatePipe } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GraphService {
-  private apiUrl = "https://localhost:44313/api/Graphs";
+  private apiUrl = "https://localhost:44313/api/Graphs/by-date-range";
 
-  constructor( private http: HttpClient) { }
+  constructor(private http: HttpClient, private datePipe: DatePipe) {}
 
-  getGraphData(): Observable<GraphData[]> {
-    return this.http.get<GraphData[]>(this.apiUrl);
+  getGraphData(startDate: Date, endDate: Date): Observable<any> {
+  const start = startDate.toISOString();  
+  const end = endDate.toISOString();  
+
+  let params = new HttpParams()
+    .set('startDate', start)
+    .set('endDate', end);
+
+  return this.http.get<any>(this.apiUrl, { params });
 }
 }
+
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
+import { DatePipe } from '@angular/common';
+import { importProvidersFrom } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(),provideAnimations()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(),provideAnimations(), DatePipe, importProvidersFrom()]
 };
